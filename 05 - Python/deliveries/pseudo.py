6fd002e4a4ffce3141e680039239440991bf5435
Diego Gómez <diego.gomez@digitalonus.com>
2021-04-13 15:48:27 -0500

    Python: tuple updated

--- 05 - Python/deliveries/pseudo.py
+++ 05 - Python/deliveries/pseudo.py
@@ -9,18 +9,16 @@ names = ()
 # Generate a tuple using dicts key 'name' with common loop
 for i in dicts:
     names = names + (i.get('name'),)
-# print(names)
+print(names)
 # Generate the same tuple with comprehension list
-namesTwo = [i.get('name') for i in dicts]
-# print(namesTwo)
-
+namesTwo = tuple(i.get('name') for i in dicts)
+print(namesTwo)
 
 # Transform that list of comprenhention into a generator and consume it.
 def generate():
+    newNames = ()
     for i in dicts:
-        print(i.get('name'))
-        yield 1
-
-
+        newNames=newNames+(i.get('name'),)
+    yield newNames
 namesThree = generate()
-print(namesThree)
+print(namesThree.__next__())
