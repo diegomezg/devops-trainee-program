d2f5313fca46e8cc027a5ad84f3520af979eec56
Diego Gómez <diego.gomez@digitalonus.com>
2021-04-13 17:10:30 -0500

    Python: 05 classes and decorators

---
+++ 05 - Python/deliveries/classes_decorators.py
@@ -0,0 +1,5 @@
+# create two classes that represents an abstract form
+# with at least 3 parameters and methods
+# initialize some objects from this classes
+# use one magic method
+# create a function decorator and a class decorator, try to make something useful
