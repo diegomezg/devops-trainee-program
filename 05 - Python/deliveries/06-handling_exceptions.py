519b9fd75efa434b0ec18f4a9f903f170a105d52
Diego Gómez <diego.gomez@digitalonus.com>
2021-04-14 11:36:09 -0500

    Python: 06 instructions

---
+++ 05 - Python/deliveries/06-handling_exceptions.py
@@ -0,0 +1,5 @@
+# Create a class with a custom exception named "DOU_Error".
+# Create a function that raises that custom exception everytime 
+# an string has any character which is not a word character.
+# Give an explanation to the class about when you should try to handle an exception
+# and when not (Examples requiered)
