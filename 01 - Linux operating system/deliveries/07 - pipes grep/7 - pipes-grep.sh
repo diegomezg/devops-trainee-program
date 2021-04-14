d1c430b643d2b31373efc99696982431eae682a4
Diego Gómez <diego.gomez@digitalonus.com>
2021-04-14 11:51:13 -0500

    Linux: 07 egrep example correction

--- 01 - Linux operating system/deliveries/07 - pipes grep/7 - pipes-grep.sh
+++ 01 - Linux operating system/deliveries/07 - pipes grep/7 - pipes-grep.sh
@@ -3,4 +3,4 @@
 #Present an example where you combine a pipe with grep and other command
 history | grep "systemctl"
 #Present an example where you combine a pipe with egrep and other command
-ls -lha | egrep "index"
+cat samplefile.txt | egrep debian samplefile.txt
