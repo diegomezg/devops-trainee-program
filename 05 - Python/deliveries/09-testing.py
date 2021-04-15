3ab83979638239adab7114949e6060b8f34e5f7c
Diego Gómez <diego.gomez@digitalonus.com>
2021-04-15 11:02:48 -0500

    Python: 09 instructions

---
+++ 05 - Python/deliveries/09-testing.py
@@ -0,0 +1,3 @@
+# Create at least 5 unit tests based on any of the excercies listed in the other modules.
+# At least one of the unit test should have positive and negative case scenario.
+# An explanation to all the class on why you did the tests like that.
